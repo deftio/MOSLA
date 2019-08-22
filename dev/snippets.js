c47a32b92877cc366bb02bd6703fe943e1ecca0a
manu <deftio@deftio.com>
2019-08-22 09:54:17 -0700

    updated demo format issue

---
+++ dev/snippets.js
@@ -0,0 +1,7 @@
+// snippets for mosla demo
+
+var randClauseNum = function() {
+		var s ="";
+		s+= "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(bw.random(1,26))+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(bw.random(1,26))+ "."+ bw.random(1,123);
+		return s;
+	}
